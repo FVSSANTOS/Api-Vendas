1d4c21e1ff75e1f7f4452d285b905c8264bf436e
FVSSANTOS <flaviovss@yahoo.com>
2023-07-10 16:48:12 -0300

    Criando primeiro end point

--- src/main/java/com/fvss/vendas/VendasApplication.java
+++ src/main/java/com/fvss/vendas/VendasApplication.java
@@ -1,11 +1,24 @@
 package com.fvss.vendas;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import com.fvss.vendas.domain.entity.Cliente;
+import com.fvss.vendas.domain.repository.Clientes;
 
 @SpringBootApplication
 public class VendasApplication {
 		
+	@Bean
+	public CommandLineRunner commandLineRunner(@Autowired Clientes clientes){
+		return args -> {
+			Cliente c = new Cliente("Fulano", null);
+			clientes.save(c);
+		};
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(VendasApplication.class, args);
 	}
--- src/main/java/com/fvss/vendas/rest/controller/ClienteController.java
+++ src/main/java/com/fvss/vendas/rest/controller/ClienteController.java
@@ -1,18 +1,38 @@
 package com.fvss.vendas.rest.controller;
 
+
+import java.util.Optional;
+
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.fvss.vendas.domain.entity.Cliente;
+import com.fvss.vendas.domain.repository.Clientes;
 
 @Controller
 @RequestMapping("/api/clientes")
 public class ClienteController {
     
-    @RequestMapping(value = "/hello/{nome}", method = RequestMethod.GET)
-    @ResponseBody
-    public String helloClientes(@PathVariable("nome") String nomeCliente){
-        return String.format("Hellor %s", nomeCliente);
+    private Clientes clientes;
+
+    public ClienteController(Clientes clientes){
+        this.clientes = clientes;
     }
+   
+    @GetMapping("/{id}")
+    @ResponseBody
+    public ResponseEntity<Cliente> getClienteById(@PathVariable Integer id){
+        Optional<Cliente> cliente = clientes.findById(id);
+
+        if(cliente.isPresent()){
+            return ResponseEntity.ok(cliente.get());
+        }
+
+        return ResponseEntity.notFound().build();
+    }
+
 }
