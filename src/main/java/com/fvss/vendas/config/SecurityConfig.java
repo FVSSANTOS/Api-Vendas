3283aa7a88af6c3e0e93410d0ce19bb6175b9cb1
FVSS <flaviovss@yahoo.com>
2023-07-24 11:17:06 -0300

    .

--- src/main/java/com/fvss/vendas/config/SecurityConfig.java
+++ src/main/java/com/fvss/vendas/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.fvss.vendas.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
@@ -10,9 +11,21 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 @Configuration
 public class SecurityConfig {
     
-    @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        //configs
+    protected void configure(HttpSecurity http) throws Exception{
+        http
+            .httpBasic()
+            .and()
+            .authorizeHttpRequests()
+            .anyRequest().authenticated()
+            .and()
+            .csrf().disable();
+    }
+
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
+        auth.inMemoryAuthentication()
+            .withUser("flavio")
+            .password(passwordEncoder().encode("34474549"))
+            .roles("ADMIN");
     }
 
     @Bean
